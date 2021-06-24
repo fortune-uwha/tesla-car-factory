8b060da11cbf31ebc8bb123118196ad60827ef4d
Fortune Uwha <fortune.uwha@gmail.com>
2021-06-24 17:20:21 +0100

    test_tesla

--- tests/test_tesla.py
+++ tests/test_tesla.py
@@ -1,11 +1,9 @@
-from setuptools import setup
+tesla2 = Tesla("S", "blue", autopilot=True)
 
-setup(
-    name='Tesla car factory',
-    version='0.0.1',
-    desription='Package for creating Tesla cars with functions like lock, charge, etc.',
-    author='Fortune Uwha',
-    author_email='fortune.uwha@gmail.com',
-    url='https://github.com/fortune-uwha/tesla_car_factory',
-    packages=['tesla']
-)
+
+def test_color():
+    assert tesla2.color == "blue"
+
+
+def test_autopilot():
+    assert tesla2.autopilot("tree") == "Tesla model S avoids tree"
